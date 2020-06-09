7fc942872767d03507102c8cb1d3768a96225d28
Gabriel Borrelli <g_borr04@uni-muenster.de>
2020-06-09 10:14:51 +0200

    Add multiple movement speeds and fix some errors

--- src/CubeMovementSystem.cpp
+++ src/CubeMovementSystem.cpp
@@ -3,7 +3,6 @@
 #include <GLFW/glfw3.h>
 
 #include <visualizer/CubeTickInfo.hpp>
-#include <visualizer/Systems.hpp>
 #include <visualizer/Transform.hpp>
 
 namespace Visualizer {
@@ -18,11 +17,57 @@ CubeMovementSystem::CubeMovementSystem()
     m_currentTime = glfwGetTime();
 }
 
+void CubeMovementSystem::initialize() { m_componentManager = m_world->getManager<ComponentManager>(); }
+
+void CubeMovementSystem::terminate() { m_componentManager = nullptr; }
+
+void tick(CubeTickInfo& cubeInfo)
+{
+    if (++cubeInfo.currentTick % cubeInfo.tickRate != 0) {
+        return;
+    } else {
+        cubeInfo.currentTick = 0;
+    }
+
+    if (cubeInfo.currentIter[cubeInfo.order[0]] < cubeInfo.limits[cubeInfo.order[0]]) {
+        cubeInfo.currentIter[cubeInfo.order[0]]++;
+    } else {
+        cubeInfo.currentIter[cubeInfo.order[0]] = 0;
+        if (cubeInfo.currentIter[cubeInfo.order[1]] < cubeInfo.limits[cubeInfo.order[1]]) {
+            cubeInfo.currentIter[cubeInfo.order[1]]++;
+        } else {
+            cubeInfo.currentIter[cubeInfo.order[1]] = 0;
+            if (cubeInfo.currentIter[cubeInfo.order[2]] < cubeInfo.limits[cubeInfo.order[2]]) {
+                cubeInfo.currentIter[cubeInfo.order[2]]++;
+            } else {
+                cubeInfo.currentIter[cubeInfo.order[2]] = 0;
+            }
+        }
+    }
+}
+
+void tick(const CubeTickInfo& cubeInfo, Transform& transform)
+{
+    auto posX{ transform.scale.x * cubeInfo.currentIter.x };
+    auto posY{ -transform.scale.y * cubeInfo.currentIter.y };
+    auto posZ{ transform.scale.z * cubeInfo.currentIter.z };
+    transform.position = cubeInfo.startPos + glm::vec3{ posX, posY, posZ };
+}
+
 void CubeMovementSystem::run(void*)
 {
     auto currentTime{ glfwGetTime() };
     auto deltaTime{ currentTime - m_currentTime };
     m_currentTime = currentTime;
+
+    auto window{ glfwGetCurrentContext() };
+    if (glfwGetKey(window, GLFW_KEY_Z) == GLFW_PRESS) {
+        m_tickInterval = 1.0f;
+    } else if (glfwGetKey(window, GLFW_KEY_X) == GLFW_PRESS) {
+        m_tickInterval = 0.1f;
+    } else if (glfwGetKey(window, GLFW_KEY_C) == GLFW_PRESS) {
+        m_tickInterval = 0.01f;
+    }
 
     m_accumulator += deltaTime;
     if (m_accumulator >= m_tickInterval) {
@@ -34,8 +79,5 @@ void CubeMovementSystem::run(void*)
             });
     }
 }
-
-void CubeMovementSystem::initialize() { m_componentManager = m_world->getManager<ComponentManager>(); }
-void CubeMovementSystem::terminate() { m_componentManager = nullptr; }
 
 }
