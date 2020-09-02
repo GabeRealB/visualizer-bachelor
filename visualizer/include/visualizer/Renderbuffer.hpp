230dfa30f8b985e487438eaa5dda6760ed29db25
Gabriel Borrelli <g_borr04@uni-muenster.de>
2020-09-02 13:56:17 +0200

    [visualizer] Add more renderbuffer formats

--- visualizer/include/visualizer/Renderbuffer.hpp
+++ visualizer/include/visualizer/Renderbuffer.hpp
@@ -4,7 +4,42 @@
 
 namespace Visualizer {
 
-enum class RenderbufferFormat { RGBA, Depth24Stencil8 };
+enum class RenderbufferFormat {
+    R8,
+    R8UI,
+    R8I,
+    R16UI,
+    R16I,
+    R32UI,
+    R32I,
+    RG8,
+    RG8UI,
+    RG8I,
+    RG16UI,
+    RG16I,
+    RG32UI,
+    RG32I,
+    RGB8,
+    RGB565,
+    RGBA8,
+    SRGB8Alpha8,
+    RGB5A1,
+    RGBA4,
+    RGB10A2,
+    RGBA8UI,
+    RGBA8I,
+    RGB10A2UI,
+    RGBA16UI,
+    RGBA16I,
+    RGBA32I,
+    RGBA32UI,
+    Depth16,
+    Depth24,
+    Depth32F,
+    Depth24Stencil8,
+    Depth32FStencil8,
+    Stencil8,
+};
 
 class Renderbuffer {
 public:
--- visualizer/src/Renderbuffer.cpp
+++ visualizer/src/Renderbuffer.cpp
@@ -44,11 +44,107 @@ void Renderbuffer::setFormat(RenderbufferFormat format, GLsizei width, GLsizei h
     GLenum formatGl;
 
     switch (format) {
-    case RenderbufferFormat::RGBA:
-        formatGl = GL_RGBA;
+    case RenderbufferFormat::R8:
+        formatGl = GL_R8;
+        break;
+    case RenderbufferFormat::R8UI:
+        formatGl = GL_R8UI;
+        break;
+    case RenderbufferFormat::R8I:
+        formatGl = GL_R8I;
+        break;
+    case RenderbufferFormat::R16UI:
+        formatGl = GL_R16UI;
+        break;
+    case RenderbufferFormat::R16I:
+        formatGl = GL_R16I;
+        break;
+    case RenderbufferFormat::R32UI:
+        formatGl = GL_R32UI;
+        break;
+    case RenderbufferFormat::R32I:
+        formatGl = GL_R32I;
+        break;
+    case RenderbufferFormat::RG8:
+        formatGl = GL_RG8;
+        break;
+    case RenderbufferFormat::RG8UI:
+        formatGl = GL_RG8UI;
+        break;
+    case RenderbufferFormat::RG8I:
+        formatGl = GL_RG8I;
+        break;
+    case RenderbufferFormat::RG16UI:
+        formatGl = GL_RG16UI;
+        break;
+    case RenderbufferFormat::RG16I:
+        formatGl = GL_RG16I;
+        break;
+    case RenderbufferFormat::RG32UI:
+        formatGl = GL_RG32UI;
+        break;
+    case RenderbufferFormat::RG32I:
+        formatGl = GL_RG32I;
+        break;
+    case RenderbufferFormat::RGB8:
+        formatGl = GL_RGB8;
+        break;
+    case RenderbufferFormat::RGB565:
+        formatGl = GL_RGB565;
+        break;
+    case RenderbufferFormat::RGBA8:
+        formatGl = GL_RGBA8;
+        break;
+    case RenderbufferFormat::SRGB8Alpha8:
+        formatGl = GL_SRGB8_ALPHA8;
+        break;
+    case RenderbufferFormat::RGB5A1:
+        formatGl = GL_RGB5_A1;
+        break;
+    case RenderbufferFormat::RGBA4:
+        formatGl = GL_RGBA4;
+        break;
+    case RenderbufferFormat::RGB10A2:
+        formatGl = GL_RGB10_A2;
+        break;
+    case RenderbufferFormat::RGBA8UI:
+        formatGl = GL_RGBA8UI;
+        break;
+    case RenderbufferFormat::RGBA8I:
+        formatGl = GL_RGBA8I;
+        break;
+    case RenderbufferFormat::RGB10A2UI:
+        formatGl = GL_RGB10_A2UI;
+        break;
+    case RenderbufferFormat::RGBA16UI:
+        formatGl = GL_RGBA16UI;
+        break;
+    case RenderbufferFormat::RGBA16I:
+        formatGl = GL_RGBA16I;
+        break;
+    case RenderbufferFormat::RGBA32I:
+        formatGl = GL_RGBA32I;
+        break;
+    case RenderbufferFormat::RGBA32UI:
+        formatGl = GL_RGBA32UI;
+        break;
+    case RenderbufferFormat::Depth16:
+        formatGl = GL_DEPTH_COMPONENT16;
+        break;
+    case RenderbufferFormat::Depth24:
+        formatGl = GL_DEPTH_COMPONENT24;
+        break;
+    case RenderbufferFormat::Depth32F:
+        formatGl = GL_DEPTH_COMPONENT32F;
         break;
     case RenderbufferFormat::Depth24Stencil8:
         formatGl = GL_DEPTH24_STENCIL8;
+        break;
+    case RenderbufferFormat::Depth32FStencil8:
+        formatGl = GL_DEPTH32F_STENCIL8;
+        break;
+    case RenderbufferFormat::Stencil8:
+        formatGl = GL_STENCIL_INDEX8;
         break;
     default:
         return;
