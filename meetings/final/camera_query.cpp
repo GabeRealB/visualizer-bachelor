088c050c2eecd51d8e67db0d361bf528aec544c6
Gabriel Borrelli <g_borr04@uni-muenster.de>
2020-10-04 21:44:36 +0200

    [meetings] Final presentation first draft

--- meetings/final/camera_query.cpp
+++
@@ -1,6 +0,0 @@
-auto camera_query = EntityQuery{}.with<Camera>();
-auto free_camera_query = EntityQuery{}.with<Camera, FreeCamera>();
-auto fixed_camera_query = EntityQuery{}.with<Camera, FixedCamera>();
-... query.query(componentManager)
-    .filter<Camera>([](const Camera* camera) -> bool { return camera->active; })
-    .forEach<...>([&](...) { ... });
