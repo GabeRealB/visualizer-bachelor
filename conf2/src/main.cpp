cb01c360c8ea388ce6c1a6f206f15980cb0b89af
Gabriel Borrelli <g_borr04@uni-muenster.de>
2021-04-23 11:44:23 +0200

    Add more flags

--- conf2/src/main.cpp
+++ conf2/src/main.cpp
@@ -8,11 +8,18 @@ struct Print {
 };
 
 struct Generate {
+    bool screen_fullscreen;
+    std::size_t screen_width;
+    std::size_t screen_height;
+    std::size_t screen_msaa_samples;
+    std::size_t render_resolution_multiplier;
     std::filesystem::path output_path;
     std::filesystem::path resource_path;
 };
 
-constexpr std::string_view usage_string = "Usage: conf2 [print | generate [-o output_path] [-r resource_path]]";
+constexpr std::string_view usage_string
+    = "Usage: conf2 [print | generate [-o output_path] [-r resource_path] [-sw screen_width] [-sh screen_height] "
+      "[-msaa msaa_samples] [-rmul resolution_multiplier] [-fs]]";
 
 using OperationVariant = std::variant<std::monostate, Print, Generate>;
 
@@ -32,6 +39,10 @@ int main(int argc, char* argv[])
         } else if (strcmp(argv[1], "generate") == 0) {
             Generate options{};
 
+            bool screen_width = false;
+            bool screen_height = false;
+            bool screen_msaa_samples = false;
+            bool render_resolution_multiplier = false;
             bool output_path = false;
             bool resource_path = false;
 
@@ -54,9 +65,67 @@ int main(int argc, char* argv[])
                     i++;
                     resource_path = true;
                     options.resource_path = std::filesystem::path{ argv[i] };
+                } else if (strcmp(argv[i], "-fs") == 0) {
+                    options.screen_fullscreen = true;
+                } else if (strcmp(argv[i], "-sw") == 0) {
+                    if (i + 1 >= argc) {
+                        std::cerr << "Too few arguments after -sw, expected 1, got 0." << std::endl;
+                        return std::monostate{};
+                    }
+
+                    i++;
+                    screen_width = true;
+                    options.screen_width = std::atoi(argv[i]);
+                } else if (strcmp(argv[i], "-sh") == 0) {
+                    if (i + 1 >= argc) {
+                        std::cerr << "Too few arguments after -sh, expected 1, got 0." << std::endl;
+                        return std::monostate{};
+                    }
+
+                    i++;
+                    screen_height = true;
+                    options.screen_height = std::atoi(argv[i]);
+                } else if (strcmp(argv[i], "-msaa") == 0) {
+                    if (i + 1 >= argc) {
+                        std::cerr << "Too few arguments after -msaa, expected 1, got 0." << std::endl;
+                        return std::monostate{};
+                    }
+
+                    i++;
+                    screen_msaa_samples = true;
+                    options.screen_msaa_samples = std::atoi(argv[i]);
+                } else if (strcmp(argv[i], "-rmul") == 0) {
+                    if (i + 1 >= argc) {
+                        std::cerr << "Too few arguments after -rmul, expected 1, got 0." << std::endl;
+                        return std::monostate{};
+                    }
+
+                    i++;
+                    render_resolution_multiplier = true;
+                    options.render_resolution_multiplier = std::atoi(argv[i]);
                 }
             }
 
+            constexpr double aspect_ration = 4.0 / 3.0;
+
+            if (!screen_width) {
+                options.screen_width = 1200;
+            } else {
+                options.screen_height
+                    = static_cast<std::size_t>(static_cast<double>(options.screen_width) / aspect_ration);
+                screen_height = true;
+            }
+            if (!screen_height) {
+                options.screen_height = 900;
+                options.screen_width
+                    = static_cast<std::size_t>(static_cast<double>(options.screen_height) * aspect_ration);
+            }
+            if (!screen_msaa_samples) {
+                options.screen_msaa_samples = 32;
+            }
+            if (!render_resolution_multiplier) {
+                options.render_resolution_multiplier = 2;
+            }
             if (!output_path) {
                 options.output_path = std::filesystem::current_path();
             }
@@ -94,12 +163,12 @@ void print_handler(const Config::ConfigCommandList& command_list) { Config::prin
 void generate_handler(const Generate& operation, const Config::ConfigCommandList& command_list)
 {
     Config::GenerationOptions generation_options{};
-    generation_options.screen_width = 1200;
-    generation_options.screen_height = 900;
-    generation_options.screen_msaa_samples = 32;
-    generation_options.screen_fullscreen = false;
+    generation_options.screen_width = operation.screen_width;
+    generation_options.screen_height = operation.screen_height;
+    generation_options.screen_msaa_samples = operation.screen_msaa_samples;
+    generation_options.screen_fullscreen = operation.screen_fullscreen;
 
-    generation_options.render_resolution_multiplier = 2;
+    generation_options.render_resolution_multiplier = operation.render_resolution_multiplier;
 
     generation_options.cuboid_border_width = 0.08f;
 
